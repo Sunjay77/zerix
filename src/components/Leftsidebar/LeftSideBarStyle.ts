c8972cdb037efbdcf45613a8d81733cc98869ced
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-03-29 10:53:29 +0545

    SideBar: Added Icons

--- src/components/Leftsidebar/LeftSideBarStyle.ts
+++ src/components/Leftsidebar/LeftSideBarStyle.ts
@@ -6,21 +6,29 @@ export const LeftSidebar = styled.div`
   height: 100vh;
   background: black;
   border: solid black;
-  color: white;
+  color: gray;
   display: flex;
-  flex: 1;
   flex-direction: column;
   gap: 0.5rem;
   border-right: 1px solid gray;
-  padding-left: 20px;
   font-family: monospace;
 `;
 export const SidebarHeading = styled.h2`
   font-weight: 100;
-  font-size: x-small;
+  font-size: small;
   color: #6f7279;
   margin-top: 20px;
-  margin-bottom: 5px;
 `;
 
-export const SidebarContent = styled.h3``;
+export const SidebarItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  padding: 7px;
+  border-radius: 10px;
+  &:hover {
+    color: white;
+    background: #1a1a1a;
+  }
+`;
--- src/components/Leftsidebar/Leftsidebar.tsx
+++ src/components/Leftsidebar/Leftsidebar.tsx
@@ -1,13 +1,18 @@
-import { LeftSidebar, SidebarHeading } from "./LeftSideBarStyle";
+import { LeftSidebar, SidebarHeading, SidebarItem } from "./LeftSideBarStyle";
+import { WindowsIcon } from "../icons/WindowsIcon";
+import { HeroIcon } from "../icons/HeroIcon";
+import { FeatureIcon } from "../icons/FeatureIcon";
+import { CtaIcon } from "../icons/CtaIcon";
+import { FooterIcon } from "../icons/FooterIcon";
 const LeftSideBar: React.FC = () => {
   return (
     <LeftSidebar>
       <SidebarHeading>COMPONENTS</SidebarHeading>
-      <h3>Navbar</h3>
-      <h3>Hero</h3>
-      <h3>Features</h3>
-      <h3>CTA</h3>
-      <h3>Footer</h3>
+       <SidebarItem><WindowsIcon /> <h3>Navbar</h3> </SidebarItem>
+        <SidebarItem> <HeroIcon /> <h3>Hero</h3> </SidebarItem>
+        <SidebarItem> <FeatureIcon /> <h3>Features</h3> </SidebarItem>
+        <SidebarItem> <CtaIcon /> <h3>CTA</h3> </SidebarItem>
+        <SidebarItem> <FooterIcon /> <h3>Footer</h3> </SidebarItem>
     </LeftSidebar>
   );
 }
