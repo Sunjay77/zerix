24724c548c148025e77c2f21d6ccc32b49723a42
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-04-01 10:12:46 +0545

    moved the file Dir

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,19 +3,20 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/elements/navbar/Navbar";
-import LeftSideBar from "../components/Leftsidebar/Leftsidebar";
-import Hero from "../components/Hero/Hero";
+import LeftSideBar from "../components/elements/leftsidebar/Leftsidebar";
+import Hero from "../components/elements/Hero/Hero";
+import RightSideBar from "../components/elements/rightsidebar/RightSideBar";
 
 const RootLayout = () => (
   <>
     <Navbar />
     <div style={{ display: "flex" }}>
       <LeftSideBar />
-      <div style={{display: "flex:1"}}>
       <Hero />
+      <RightSideBar />
       <Outlet />
-      </div>
     </div>
+
     <TanStackRouterDevtools />
   </>
 );
