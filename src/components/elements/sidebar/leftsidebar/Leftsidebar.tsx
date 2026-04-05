985db0c9e059b05a23aee1666a9c843696146ebe
Sunjay77 <sanjaylamaa77@gmail.com>
2026-04-05 17:07:25 +0545

    Fix formatting inconsistencies in LeftSideBar component

--- src/components/elements/sidebar/leftsidebar/Leftsidebar.tsx
+++ src/components/elements/sidebar/leftsidebar/Leftsidebar.tsx
@@ -12,26 +12,26 @@ const LeftSideBar: React.FC = () => {
   function handleNavClick(id: string): void {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   }
 
   return (
     <LeftSidebar>
       <SidebarHeading>COMPONENTS</SidebarHeading>
-      <SidebarItem onClick={() => handleNavClick('navbar')}>
+      <SidebarItem onClick={() => handleNavClick("navbar")}>
         <WindowsIcon /> <h3>Navbar</h3>
       </SidebarItem>
-      <SidebarItem onClick={() => handleNavClick('hero')}>
+      <SidebarItem onClick={() => handleNavClick("hero")}>
         <HeroIcon /> <h3>Hero</h3>
       </SidebarItem>
-      <SidebarItem onClick={() => handleNavClick('features')}>
+      <SidebarItem onClick={() => handleNavClick("features")}>
         <FeatureIcon /> <h3>Features</h3>
       </SidebarItem>
-      <SidebarItem onClick={() => handleNavClick('cta')}>
+      <SidebarItem onClick={() => handleNavClick("cta")}>
         <CtaIcon /> <h3>CTA</h3>
       </SidebarItem>
-      <SidebarItem onClick={() => handleNavClick('footer')}>
+      <SidebarItem onClick={() => handleNavClick("footer")}>
         <FooterIcon /> <h3>Footer</h3>
       </SidebarItem>
     </LeftSidebar>
