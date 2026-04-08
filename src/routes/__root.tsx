9bc0e39c72b3915955501329b62948b95137e367
Sunjay77 <sanjaylamaa77@gmail.com>
2026-04-08 15:30:55 +0545

    fixed logic

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import React from "react";
+import { createRootRoute, Outlet, useLocation } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/elements/navbar/Navbar";
 import LeftSideBar from "../components/elements/sidebar/leftsidebar/Leftsidebar";
 import RightSideBar from "../components/elements/sidebar/rightsidebar/RightSideBar";
+import styled from "styled-components";
+
+const MainContent = styled.div<{ ispreview?: boolean }>`
+  width: 100%;
+  height: 100vh;
+  display: ${(props) => (props.ispreview ? "block" : "flex")};
+`;
 
 const RootLayout = () => {
-  const [isPreview, setIsPreview] = useState<boolean>(false);
-
+  const location = useLocation();
+  const isPreview = location.pathname === "/preview";
+  console.log(isPreview);
   return (
     <React.Fragment>
-      <Navbar isPreview={isPreview} setIsPreview={setIsPreview} />
-      <div style={{ display: "flex", width: "100%" }}>
-        <LeftSideBar />
+      {!isPreview && <Navbar />}
+      <MainContent ispreview={isPreview}>
+        {!isPreview && <LeftSideBar />}
         <Outlet />
-        <RightSideBar />
-      </div>
+        {!isPreview && <RightSideBar />}
+      </MainContent>
       <TanStackRouterDevtools />
     </React.Fragment>
   );
