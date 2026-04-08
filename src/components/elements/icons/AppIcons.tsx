5586c3d762fd76caff57ebb042426dc3e1bc018d
Sunjay77 <sanjaylamaa77@gmail.com>
2026-04-08 16:00:25 +0545

    Added: Hamburger Icon

--- src/components/elements/icons/AppIcons.tsx
+++ src/components/elements/icons/AppIcons.tsx
@@ -185,3 +185,24 @@ export const UpIcon = ({ size = 30, className }: IconProps) => {
     </svg>
   );
 };
+
+export const BurgerIcon = ({ size = 24, className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M4 5h16" />
+      <path d="M4 12h16" />
+      <path d="M4 19h16" />
+    </svg>
+  );
+};
